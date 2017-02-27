1e27e05cd9f9ffe0b5f535846b78a07535b5bf8f
Adam Cook <Adam.Cook@bbc.co.uk>
2017-02-27 11:03:35 +0000

    Make melanite use array of Matchers * Change test schema to be an array of matchers * 'Revert' changes to work with array of matchers

--- test/matcher.js
+++ test/matcher.js
@@ -6,18 +6,19 @@ describe('Device Matching', () => {
   let testData
   let matchUserAgents
   beforeEach(() => {
-    testData = {
-      'flux-profesh': {
-        invariants: [ 'Mozzarella', 'ultron' ],
-        disallowed: [],
-        fuzzy: 'Mozzarella reinstall flashy ultron; 81648/ii/AMD t0p_kek123; powered_by_NASA'
-      },
-      'google-chrome': {
-        invariants: [ 'Chrome' ],
-        disallowed: [ 'Firefox' ],
-        fuzzy: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
-      }
-    }
+    testData = [{
+      brand: 'flux',
+      model: 'profesh',
+      invariants: [ 'Mozzarella', 'ultron' ],
+      disallowed: [],
+      fuzzy: 'Mozzarella reinstall flashy ultron; 81648/ii/AMD t0p_kek123; powered_by_NASA'
+    }, {
+      brand: 'google',
+      model: 'chrome',
+      invariants: [ 'Chrome' ],
+      disallowed: [ 'Firefox' ],
+      fuzzy: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
+    }]
     matchUserAgents = match(testData)
   })
   it('Finds the correct device', function () {
@@ -29,6 +30,6 @@ describe('Device Matching', () => {
   })
   it('Returns generic device if the device is not found', () => {
     const modifiedFirefoxUA = 'Mozilla/5.0 (Macintosh; Intel Mac OS Y 10.11; rv:50.0) Gecko/20100101 Firefox/50.0 Chromelike but not/Safari/536.36'
-    expect(matchUserAgents(modifiedFirefoxUA)).to.equal(null)
+    expect(matchUserAgents(modifiedFirefoxUA)).to.deep.equal({brand: 'generic', model: 'device'})
   })
 })
