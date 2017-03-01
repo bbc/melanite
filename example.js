6e3afdfb004dc6306868a9aed9bb0939748bec27
Joel Howard <subsidel@gmail.com>
2017-03-01 19:38:02 +0000

    Update example.js
    Make example more example-y and take an array rather than the big object thing

--- example.js
+++ example.js
@@ -1,17 +1,21 @@
 const {match} = require('./')
 
-const testData = {
-  'gamestick-streamer_2013': {
+const testData = [
+  {
+    brand: 'gamestick',
+    model: 'streamer_2013',
     invariants: ['GameStick', 'Android'],
     disallowed: [],
     fuzzy: 'Dalvik/1.6.0 (Linux; U; Android 4.1.2; GameStick V1.0 Build/V1.03.04MX01_20130521)'
   },
-  'google-chrome': {
+  {
+    brand: 'google',
+    model: 'chrome',
     invariants: ['Chrome'],
     disallowed: ['ultra cool mode'],
     fuzzy: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
   }
-}
+]
 
 const chrome = 'Mozilla/5.0 (Macintosh) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
 const maybeAChrome = 'Asdf Jekul (123;456) Chrome ultra cool mode (stuff/v8)'
